7f0e93c556ade04082b8745e094d6b423501a13e
rutatulis13 <62666061+rutatulis13@users.noreply.github.com>
2022-02-18 00:13:48 +0200

    change loader, dlete common2.js file

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,27 +2,47 @@ let page = 1;
 let isLoading = {loading: true};
 
 const imageLoader = () => {
-fetch('https://picsum.photos/v2/list?page=' + page + '&limit=10/')
-.then((response) => response.json())
-.then(jsonObjektas => {
-    console.log(jsonObjektas)
-    jsonObjektas.forEach((val, key) => {
-    document.querySelector(".grid").innerHTML += `<div class="overlay"><div class="grid-item"><img src="${val.download_url}" /></div></div>`
-    })
-    isLoading.loading = false;
-})
+  let limit = 10;
+  document.querySelector('.loader').style.display = 'block';
+  
+  fetch('https://picsum.photos/v2/list?page=' + page + '&limit=' + limit)
+  .then((response) => response.json())
+  .then(jsonObjektas => {
+
+      //console.log(jsonObjektas)
+      let loaded = 0;
+      let grid = document.querySelector(".grid");
+      jsonObjektas.forEach((val, key) => {
+        let image = new Image();
+        image.src = val.download_url;
+        image.onload = async () => {
+          let overlay = document.createElement('div');
+          overlay.classList.add('overlay');
+          grid.append(overlay);
+
+          let gridItem = document.createElement('div');
+          gridItem.classList.add('grid-item');
+          overlay.append(gridItem);
+
+          gridItem.append(image);
+
+          loaded++;
+          if(loaded >= limit)
+            document.querySelector('.loader').style.display = 'none';
+        }
+        //document.querySelector(".grid").innerHTML += `<div class="overlay"><div class="grid-item"><img src="${val.download_url}" /></div></div>`
+      });
+
+      isLoading.loading = false;
+  })
 }
 imageLoader();
 
-document.querySelector(".loadmore").addEventListener("click", () => {
-    setTimeout(remove, 3000)
-      }
-    )
 
-      function remove() {
-        page++;
-        imageLoader();
-      }
+document.querySelector(".loadmore").addEventListener("click", () => {
+  page++;
+  imageLoader();
+})
 
 window.addEventListener("scroll", () => {
     let btnPos = document.querySelector(".buttonHolder").offsetTop;
@@ -32,6 +52,7 @@ window.addEventListener("scroll", () => {
         imageLoader()
     }
 })
+    
     
     
 
